4865f81e0ac9ea7c6b43b8a884a4cb417b934a5c
lab_1_1x <lab_1_15@lab-1-15s-iMac.local>
2022-04-07 10:04:18 +0800

    haha

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const app = express()
 const PORT = 3000
 const routes = require('./routes/api')
 const connection = require('./database')
+const cors = require('./middleware/cors')
+
 
 app.use(express.json())
 app.use(express.static("static"))
-app.use("/", routes)
+app.use("/", cors ,routes)
 app.all("*", (req,res,next)=>{
     res.status(404).json({
         success:false,
